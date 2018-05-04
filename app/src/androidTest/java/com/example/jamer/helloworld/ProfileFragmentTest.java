2c47358e95add231a947fa14e2a8f1a087fffbc5
Jeremiah Smith <jsmit028@seattlecentral.edu>
2018-05-03 18:19:28 -0700

    HW4 second commit--testing

--- app/src/androidTest/java/com/example/jamer/helloworld/ProfileFragmentTest.java
+++ app/src/androidTest/java/com/example/jamer/helloworld/ProfileFragmentTest.java
@@ -8,7 +8,14 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class ProfileFragmentTest {
@@ -33,17 +40,14 @@ public class ProfileFragmentTest {
     };
 
 
-//    @Test
-//    public void testThatImageExists() {
-//        onView(withId(R.id.img))
-//                .check(matches(isDisplayed()));
-//    }
-//
-//    @Test
-//    public void testThatNameExists() {
-//        onView(withId(R.id.profilename))
-//                .check(matches(isDisplayed()));
-//    }
+    @Test
+    public void testForTabs(){
+
+        onView(withId(R.id.viewpager))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+    }
 
 //    @Test
 //    public void testIntentExtra(){
