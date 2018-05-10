dc1c644ae8866973ae99ea831916faecbd56e283
Jeremiah Smith <jsmit028@seattlecentral.edu>
2018-05-09 22:59:24 -0700

    HW 5 first commit

--- app/src/main/java/com/example/jamer/helloworld/MatchesFragment.java
+++ app/src/main/java/com/example/jamer/helloworld/MatchesFragment.java
@@ -1,11 +1,23 @@
 package com.example.jamer.helloworld;
 
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -19,11 +31,91 @@ public class MatchesFragment extends Fragment {
     }
 
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_matches, container, false);
+//        return inflater.inflate(R.layout.fragment_matches, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+
     }
 
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mPicture;
+        public TextView mName;
+        public TextView mDescription;
+        public ImageButton likeButton;
+        boolean flag = false;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.fragment_matches, parent, false));
+
+            mPicture = itemView.findViewById(R.id.card_image);
+            mName = itemView.findViewById(R.id.card_title);
+            mDescription = itemView.findViewById(R.id.card_text);
+            likeButton = itemView.findViewById(R.id.likeButton);
+
+            likeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!flag) {
+                        Toast.makeText(itemView.getContext(), "You liked " + mName.getText().toString(), Toast.LENGTH_LONG).show();
+                        flag = true;
+                        likeButton.setColorFilter(Color.RED);
+                    } else {
+                        flag = false;
+                        likeButton.setColorFilter(Color.GRAY);
+                    }
+                }
+            });
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+
+        private final String[] mNames;
+        private final String[] mDescriptions;
+        private final Drawable[] mPictures;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mNames = resources.getStringArray(R.array.matches_names);
+            mDescriptions = resources.getStringArray(R.array.matches_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.matches_picture);
+            mPictures = new Drawable[a.length()];
+            for (int i = 0; i < mPictures.length; i++) {
+                mPictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mPicture.setImageDrawable(mPictures[position % mPictures.length]);
+            holder.mName.setText(mNames[position % mNames.length]);
+            holder.mDescription.setText(mDescriptions[position % mDescriptions.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNames.length;
+        }
+
+    }
 }
+
