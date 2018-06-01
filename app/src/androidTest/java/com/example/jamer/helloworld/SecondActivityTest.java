1e7d8d90c115d9a06ac0ac92c91d2f5f005d53ee
Jeremiah Smith <jsmit028@seattlecentral.edu>
2018-05-31 23:15:19 -0700

    HW8 matches fragment is blank

--- app/src/androidTest/java/com/example/jamer/helloworld/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/jamer/helloworld/SecondActivityTest.java
@@ -6,14 +6,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class SecondActivityTest {
@@ -38,14 +31,14 @@ public class SecondActivityTest {
     };
 
 //Passed
-    @Test
-    public void testForTabs(){
-
-        onView(withId(R.id.viewpager))
-                .perform(click())
-                .check(matches(isDisplayed()));
-
-}
+//    @Test
+//    public void testForTabs(){
+//
+//        onView(withId(R.id.viewpager))
+//                .perform(click())
+//                .check(matches(isDisplayed()));
+//
+//}
 
 
 //    @Test
